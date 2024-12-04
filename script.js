9e5e2e9f4c318daf063b1158bf805bda1e081f24
habeel bhatti <habeelbhatti873@gmail.com>
2024-12-04 23:10:53 +0500

    add new events

---
+++ script.js
@@ -0,0 +1,7 @@
+ let button=document.getElementById("btn");
+ button.addEventListener("click",()=>{
+     document.querySelector(".box").innerHTML="you can change me HAAA";
+ })
+ button.addEventListener("contextmenu",()=>{
+ alert("onlt accecpt right click please")
+ })
